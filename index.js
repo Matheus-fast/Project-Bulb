2a38d2816a017502398b9d5f170a8c1278efb36d
Matheus_-_- <matheusoliveirale2007@gmail.com>
2024-04-14 17:16:42 -0300

    projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+function changeBackgroundDark() {
+
+    var body = document.body;
+    body.style.backgroundColor = '#5c2a2b';
+    document.getElementById('bulbOff').src='./img/bulbOn.png';
+
+}
+
+function changeBackgroundWhite() {
+    
+    var body = document.body;
+    body.style.backgroundColor = '#f7eee9';
+    document.getElementById('bulbOff').src='./img/bulbOff.png';
+}
